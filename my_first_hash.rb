58b321baf4cdff842592081656dfdd1509de5170
Scott Jordan <scottjordanswimming@gmail.com>
2019-09-22 18:15:54 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -30,15 +30,9 @@ def adding
                  }
                  
                  shipping_manifest["muskets"] = 2 
-                 
-  # add 2 muskets to the shipping_manifest hash below
+                 shipping_manifest["gun powder"] = 4
+              
+              shipping_manifest
 
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
 
 end
